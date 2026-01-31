8a3bf64563f0348e663458c54a5829f61077ba91
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-01-31 22:49:22 +0530

    Create quiz_screen.dart

---
+++ frontend/screens/quiz_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class QuizScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Quiz")),
+      body: Center(child: Text("Quiz questions here")),
+    );
+  }
+}
