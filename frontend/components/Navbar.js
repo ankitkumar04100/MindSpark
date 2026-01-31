18189969b673b65d354be074edeb88bf775ad9eb
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-01-31 22:42:49 +0530

    Create Navbar.js

---
+++ frontend/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav style={{ padding: 15, background: "#1e90ff", color: "white" }}>
+      <h2>MindSpark</h2>
+    </nav>
+  );
+}
