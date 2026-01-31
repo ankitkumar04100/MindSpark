4dcc6fc9939adce69bd9262d68c3923cd36837a0
ankitkumar04100 <ankitkumarforall@gmail.com>
2026-01-31 22:58:23 +0530

    Create recommendations.py

---
+++ ai/analytics_ai/recommendations.py
@@ -0,0 +1,7 @@
+def recommend_next_lesson(level):
+    if level == 1:
+        return "Basic concepts revision"
+    elif level == 2:
+        return "Intermediate problem solving"
+    else:
+        return "Advanced application challenges"
